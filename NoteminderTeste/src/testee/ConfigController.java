f68c9748dfd1ee59ea51b3fad4aca94866e6c532
Yara Azevedo <yara.zs@hotmail.com>
2020-10-06 20:17:16 -0300

    noteminder

---
+++ NoteminderTeste/src/testee/ConfigController.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testee;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author yaraz
+ */
+public class ConfigController implements Initializable {
+
+    @FXML private ResourceBundle resources;
+
+    @FXML private URL location;
+
+    @FXML private Button setconfig;
+
+    @FXML private Button setnotas;
+
+    @FXML private Button setagenda;
+    
+    @FXML private Button home;
+
+    @FXML
+    void setnotas(ActionEvent event) throws IOException {
+           Parent outra = FXMLLoader.load(getClass().getResource("Notas.fxml"));
+           Scene outraa= new Scene(outra);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(outraa);
+           window.show();
+    }
+
+    @FXML
+    void setagenda(ActionEvent event) throws IOException {
+        Parent outra = FXMLLoader.load(getClass().getResource("Agenda.fxml"));
+           Scene outraa= new Scene(outra);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(outraa);
+           window.show();
+    }
+
+    @FXML
+    void setconfig(ActionEvent event) throws IOException {
+        Parent outra = FXMLLoader.load(getClass().getResource("Config.fxml"));
+           Scene outraa= new Scene(outra);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(outraa);
+           window.show();
+    }
+    
+    @FXML
+    void sethome(ActionEvent event) throws IOException {
+           Parent outra = FXMLLoader.load(getClass().getResource("FXMLDocumentController.fxml"));
+           Scene outraa= new Scene(outra);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(outraa);
+           window.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ NoteminderTeste/src/testee/FXMLDocumentController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testee;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author yaraz
+ */
+public class FXMLDocumentController implements Initializable {
+    
+     @FXML private Button button13;
+
+    @FXML private Button button11;
+
+    @FXML private Button button1;
+    
+    
+
+    @FXML
+    void TelaNotas(ActionEvent event) throws IOException {
+        Parent tela2 = FXMLLoader.load(getClass().getResource("Notas.fxml"));
+        Scene teladois = new Scene(tela2);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(teladois);
+        window.show();
+        
+    }
+
+    @FXML
+    void TelaAgenda(ActionEvent event) throws IOException {
+        Parent tela2 = FXMLLoader.load(getClass().getResource("Agenda.fxml"));
+        Scene teladois = new Scene(tela2);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(teladois);
+        window.show();
+    }
+
+    @FXML
+    void TelaConfiguracoes(ActionEvent event) throws IOException {
+        Parent tela2 = FXMLLoader.load(getClass().getResource("Config.fxml"));
+        Scene teladois = new Scene(tela2);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(teladois);
+        window.show();
+    }
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
